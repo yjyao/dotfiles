1efec34f8f285b16bd927e0f83b10be26675015b
Jy <yjyao1112@gmail.com>
2019-11-07 00:38:57 -0800

    install: Add fatal() for fatal messages.

--- install.sh
+++ install.sh
@@ -8,15 +8,16 @@ BACKUP_DIR="$DOTFILES_DST_DIR/.dotfiles.backup"
 
 has_backup=
 
-if [[ ! $SHELL =~ .*bash ]]; then
-  >&2 echo "These dotfiles are for bash only; You are running $SHELL."
+fatal() {
+  >&2 echo "$(basename "$0"): $1"
   exit 1
-fi
+}
 
-if [ ! -d "$DOTFILES_SRC_DIR" ]; then
-  >&2 echo "Cannot find $DOTFILES_SRC_DIR. Pleas run the script under dotfiles/ repo."
-  exit 1
-fi
+[[ $SHELL =~ .*bash ]] || fatal \
+  "These dotfiles are for bash only; You are running $SHELL."
+
+[ -d "$DOTFILES_SRC_DIR" ] || fatal \
+  "Cannot find $DOTFILES_SRC_DIR. Pleas run the script under dotfiles/ repo."
 
 # Use -mindepth 1 to exclude "$DOTFILES_SRC_DIR" itself.
 for path in $(find "$DOTFILES_SRC_DIR" -mindepth 1 -maxdepth 1); do
