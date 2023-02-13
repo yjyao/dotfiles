6603402f598427fa4df5520a27059cdd4349f53a
Jy Yuan <jyyuan@google.com>
2023-02-13 12:22:57 -0800

    bashrc: Add a `coalesce_f` util function.

--- bash/.bash/functions/misc.sh
+++ bash/.bash/functions/misc.sh
@@ -2,3 +2,12 @@
 md5sum() {
   "$(which md5sum)" "$@" | { (( $# > 1 )) && cat || awk '{ print $1 }'; }
 }
+
+# Coalesces values, keeping the first where `func(val)` returns 0.
+coalesce_f() {
+  local func="${1?}"; shift
+  for var in "$@"; do
+    "${func}" "${var}" && { echo "${var}"; return; }
+  done
+  return 1
+}
