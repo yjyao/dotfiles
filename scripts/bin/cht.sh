6aee4c497c02bad793f9ed442c08987629c1756e
Jy Yuan <jyyuan@google.com>
2023-02-24 13:04:54 -0800

    bin: Minor improvements to `cht.sh`.

--- scripts/bin/cht.sh
+++ scripts/bin/cht.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-: ${PAGER:=less}
-# Don't send results to a pager if being piped.
-[ -t 1 ] || PAGER=cat
+# If being piped, pipe the output through with `cat`.
+# Otherwise uses a $PAGER (default to `less`).
+[ -t 1 ] && PAGER="${PAGER:-less}" || PAGER=cat
 
 chtsh() {
+  IFS=+
+  local query="$*"
   # Print a half-screen wide horizontal devider bar
   # to mark the beginning of the results
   # because it is easy to get lost in the paragraphs and colors.
+  echo "https://cht.sh/$query"
   printf '%*s\n' "$(( ${COLUMNS:-$(tput cols)} / 2 ))" '' | tr ' ' -
-  IFS=+
-  curl -s "cht.sh/$*"
+  curl -s "cht.sh/$query"
 }
 
-chtsh "$@" | $PAGER
+chtsh "$@" | "$PAGER"
