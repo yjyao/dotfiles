ab2883074fae351b4af56938d2a55f069a4539f8
Jy Yuan <jyyuan@google.com>
2023-12-11 23:55:36 -0800

    tmux: Clean up debug messages from fastcopy-handler.sh.

--- tmux/.tmux/scripts/fastcopy-handler.sh
+++ tmux/.tmux/scripts/fastcopy-handler.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-echo > /tmp/fastcopy-handler.log
-echo "args: $@" >> /tmp/fastcopy-handler.log
-# echo "stdin: $(cat)" >> /tmp/fastcopy-handler.log
-echo "pane id: $FASTCOPY_TARGET_PANE_ID" >> /tmp/fastcopy-handler.log
-
 content="$(cat)"
 [[ $content ]] || exit
-printf -- "$content" | tmux load-buffer - && tmux paste-buffer -t "${FASTCOPY_TARGET_PANE_ID}"
+tmux set-buffer -- "$content" && tmux paste-buffer -t "${FASTCOPY_TARGET_PANE_ID}"
